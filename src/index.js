9befe68763246f04007df47b7760d26a4ce7504a
andresrokp <afrokap@gmail.com>
2023-11-22 19:20:11 -0500

    feat: hello world home page serving

--- src/index.js
+++ src/index.js
@@ -1,8 +1,11 @@
-
+// load my config
 require('dotenv').config();
-const app = require('./server');
 const PORT = process.env.PORT || 3000;
+// load core set up of the server from neighbor file
+const app = require('./server');
 
+
+// execute server object
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
--- src/server.js
+++ src/server.js
@@ -1,14 +1,26 @@
+"use strict";
 
+// load libraries
+const path = require('path');
 const express = require('express');
 const app = express();
 
+
+
+// indicate json parsing
 app.use(express.json());
 
+// to use the public folder for page serving
+const publicPath = path.resolve(__dirname, 'public');
+console.log(publicPath);
+app.use(express.static("./src/public"));
+
 // Apply custom middlewares
-app.use(require('./middleware/domainValidator'));
-app.use(require('./middleware/tokenValidator'));
+// app.use(require('./middleware/domainValidator'));
+// app.use(require('./middleware/tokenValidator'));
 
 // Apply routes
 app.use('/api', require('./routes/api'));
 
+//  object to use as server model
 module.exports = app;
