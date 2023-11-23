26a1e785c7f7e2c53ae0bcde1c0b9cd6e9f37c10
andresrokp <afrokap@gmail.com>
2023-11-22 22:40:52 -0500

    feat: buttons toggle

--- src/public/script.js
+++ src/public/script.js
@@ -1,1 +1,25 @@
 document.getElementById("title").innerText = "Hello akap's SH AIrport assistant";
+
+// This happens only after all html is loades (styles and assets does not matters)
+document.addEventListener('DOMContentLoaded',()=>{
+
+    // GRAB ELEMENTS
+    const recordButton = document.getElementById("recordButton");
+    const stopButton = document.getElementById("stopButton");
+    const textResponse = document.getElementById("textResponse");
+    const audioResponse = document.getElementById("audioResponse");
+
+    // actions for RECORD
+    recordButton.addEventListener('click', ()=>{
+        // toggle logic
+        recordButton.disabled = true;
+        stopButton.disabled = false;
+    })
+
+    // actions for STOP
+    stopButton.addEventListener('click',()=>{
+        // toggle logic
+        recordButton.disabled = false;
+        stopButton.disabled = true;
+    })
+})
