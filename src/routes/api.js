a9d6e46ab7bfa7ecf513b03e91048e1394d26f14
andresrokp <afrokap@gmail.com>
2023-11-29 13:44:08 -0500

    feat: transcript with whisperai, and into client

--- src/routes/api.js
+++ src/routes/api.js
@@ -1,29 +1,35 @@
+require('dotenv').config();
 const express = require('express');
 const router = express.Router();
 // an upload handler middleware. Is the executed form of the library;
 const upload = require('multer')();
 // to handle file system
 const fs = require('fs');
+// the all mighty power
+const OpenAI = require('openai').OpenAI;
+const openai = new OpenAI({ apiKey: process.env.OPENAI_KEY });
 
-router.post('/audiorequest', upload.single('audioFile') , (req, res) => {
+router.post('/audiorequest', upload.single('audioFile') , async (req, res) => {
   // extract middleware result
   const audioFileBuffer = req.file.buffer;
   // save as file in folder
-  fs.writeFile('./src/others/dummyFiles/requestAudioFile.wav', audioFileBuffer, (err) => {
+  fs.writeFileSync('./src/others/dummyFiles/requestAudioFile.wav', audioFileBuffer, (err) => {
     if (err) {
         console.error('Error saving audio file :( \n', err);
         res.status(500).json({ error: 'Internal Server Error :(' });
     } else {
         console.log('Audio file saved successfully :)');
-    }
+      }
+  });
+  
+  //TODO: pipeline this and avoid to use the dummy file
+  const transcription = await openai.audio.transcriptions.create({
+    file: fs.createReadStream('./src/others/dummyFiles/requestAudioFile.wav'),
+    model: 'whisper-1',
   });
 
-  // Handle income data and build a prompt
-  console.log(audioFileBuffer);
-  // Send a query to OpenAI
-  
-  // Return the response
-  res.json({ backAns: audioFileBuffer });
+  console.log('Audio transcribed :) \n', transcription);
+  res.json({backResponse: transcription})
 });
 
 module.exports = router;
