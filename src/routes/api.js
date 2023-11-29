79d0abb5fcf0d904090919a9924a59320009949c
andresrokp <afrokap@gmail.com>
2023-11-29 11:41:34 -0500

    chore: install multer and use to buffer audio file

--- src/routes/api.js
+++ src/routes/api.js
@@ -1,13 +1,16 @@
 const express = require('express');
 const router = express.Router();
+// an upload handler middleware. Is the executed form of the library;
+const upload = require('multer')();
 
-router.post('/audiorequest', (req, res) => {
+router.post('/audiorequest', upload.single('audioFile') , (req, res) => {
+  const audioFileBuffer = req.file.buffer;
   // Handle income data and build a prompt
-  console.log(req);
+  console.log(audioFileBuffer);
   // Send a query to OpenAI
   
   // Return the response
-  res.json({ llmApiMsg: 'Response from OpenAI' });
+  res.json({ backAns: audioFileBuffer });
 });
 
 module.exports = router;
