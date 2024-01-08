1b4ade5e22894f23acd3f55a6b116b2d36ae5345
andresrokp <afrokap@gmail.com>
2024-01-08 16:53:01 +0000

    feat: enabled https connection Conflict with rebase

--- src/index.js
+++ src/index.js
@@ -5,10 +5,12 @@ const app = require('./server'); // Server setup logic
 const PORT = process.env.PORT || 3000;
 
 if (process.env.NODE_ENV === 'production') {
+  console.log('production ssl server running');
   const options = {
     key: fs.readFileSync('/etc/letsencrypt/live/shassistant.ddns.net/privkey.pem'),
     cert: fs.readFileSync('/etc/letsencrypt/live/shassistant.ddns.net/fullchain.pem'),
   };
+  console.log(options.key.slice(0,20), options.cert.slice(0,20));
 
   const httpsServer = https.createServer(options, app);
 
