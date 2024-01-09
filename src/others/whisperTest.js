65747e41ef01a6d01fb042c74645c2520c72842e
andresrokp <afrokap@gmail.com>
2024-01-09 10:14:29 -0500

    fix: inital promt to induce punctuation

--- src/others/whisperTest.js
+++ src/others/whisperTest.js
@@ -7,7 +7,8 @@ const openai = new OpenAI({ apiKey: process.env.OPENAI_KEY });
 log('Fetching transcript...')
 openai.audio.transcriptions.create({
     file: fs.createReadStream('./src/dummyFiles/sampleAudio.mp3'),
-    model: 'whisper-1'
+    model: 'whisper-1',
+    prompt: 'Saludos. El siguiente es un discurso, a veces lento, a veces rápido, para ser transcrito...'
   })
   .then((transcription)=>{
       log('Sample Audio transcript :) \n', transcription);
