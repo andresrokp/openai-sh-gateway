49b77d5dd964fc80e453045d71b778292dadfb8f
andresrokp <afrokap@gmail.com>
2024-01-10 19:54:03 -0500

    chore: wrap paths

--- src/index.js
+++ src/index.js
@@ -7,10 +7,9 @@ const PORT = process.env.PORT || 3000;
 if (process.env.NODE_ENV === 'production') {
   console.log('production ssl server running');
   const options = {
-    key: fs.readFileSync('/etc/letsencrypt/live/shassistant.ddns.net/privkey.pem'),
-    cert: fs.readFileSync('/etc/letsencrypt/live/shassistant.ddns.net/fullchain.pem'),
+    key: fs.readFileSync(process.env.PRIVKEY_PATH),
+    cert: fs.readFileSync(process.env.FULLCHAIN_PATH),
   };
-  console.log(options.key.slice(0,20), options.cert.slice(0,20));
 
   const httpsServer = https.createServer(options, app);
 
